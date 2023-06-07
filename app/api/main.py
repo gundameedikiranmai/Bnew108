70dfc3572a8b7ab35018c0764de99ad449dd1f72
Dhruv Mishra <ubiquitiousdm@gmail.com>
2023-06-07 16:21:19 +0530

    add cors

--- app/api/main.py
+++ app/api/main.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import os
 import requests
@@ -18,6 +19,20 @@ class WebhookRequest(BaseModel):
     message: str
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # ---------------------
 # Health check endpoint
